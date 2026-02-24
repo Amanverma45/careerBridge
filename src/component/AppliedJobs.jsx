1c9d68c5d42c956926408e629bdd274a3c96c3f7
AmanVerma45 <av478136@gmail.com>
2026-02-24 12:28:58 +0530

    small change in appliedjobs

--- src/component/AppliedJobs.jsx
+++ src/component/AppliedJobs.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-
+// console.log("USER:", user)
 function AppliedJobs() {
   const [appliedJobs, setAppliedJobs] = useState([])
 
